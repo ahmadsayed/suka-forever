608c1dda2d766d66654a1cb3ce8abfcb186f1729
Ahmed Sayed <ahmad.ahmadsayed@gmail.com>
2022-12-28 23:57:50 +0800

    use cloudflare-ipfs in all IPFS references

--- frontend/src/js/home.js
+++ frontend/src/js/home.js
@@ -57,7 +57,7 @@ window.addEventListener('load', async event => {
         camera.setTarget(new BABYLON.Vector3(-3, 0 + (this.scrollY / (scrollHeight / 7)), 0));
 
         let stop = false;
-        BABYLON.SceneLoader.ImportMesh("", "https://bafybeicifqtj2guv3ylfbiybrt2wwsi5xjflygsnmjtlt3foasitjc7l7q.ipfs.w3s.link/ipfs/bafybeicifqtj2guv3ylfbiybrt2wwsi5xjflygsnmjtlt3foasitjc7l7q/", "intro.glb", scene, function (newMeshes, particleSystems, skeletons, animationGroups) {
+        BABYLON.SceneLoader.ImportMesh("", "https://cloudflare-ipfs.com/ipfs/bafybeicifqtj2guv3ylfbiybrt2wwsi5xjflygsnmjtlt3foasitjc7l7q/", "intro.glb", scene, function (newMeshes, particleSystems, skeletons, animationGroups) {
             newMeshes.forEach(mesh => {
             })
             animationGroups.forEach(animation => {
