291c4b08f9edc71b54895a2289880306cdcf0b0b
Ahmed Sayed <ahmad.ahmadsayed@gmail.com>
2023-01-11 02:52:37 +0800

    Fine tune Editor screen

--- frontend/dist/js/builder.js
+++ frontend/dist/js/builder.js
@@ -2,20 +2,46 @@ window.addEventListener('DOMContentLoaded', async event => {
     const canvas = document.getElementById("renderCanvas"); // Get the canvas element
     const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
     var scene = new BABYLON.Scene(engine);
-    var advancedTexture = null;
-    const WIDTH = 100, HEIGHT=50, TOP_FACTOR=1;
-    var button = BABYLON.GUI.Button.CreateImageButton("downlaod", "Download", "assets/img/download.png");
+    var advancedTexture =  BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+    const  TOP_FACTOR=0.8;
+
     var picker = null;
     var pickerTitle = null;
+    var rightClick = BABYLON.GUI.Button.CreateImageButton("rightClick", "Right click to change color", "assets/img/rightClick.png");
+    var leftClick = BABYLON.GUI.Button.CreateImageButton("leftClick", "Left click and Drag to rotate", "assets/img/leftClick.png");
+    var button = BABYLON.GUI.Button.CreateImageButton("Save", "Save GLB to disk", "assets/img/save.png");
+    function guideLocation() {
+        const GUIDE_WIDTH = 300, GUIDE_HEIGHT=100;
+        rightClick.width = `${GUIDE_WIDTH}px`;
+        rightClick.height = `${GUIDE_HEIGHT}px`;
+        rightClick.color = "white";
+        rightClick.thickness = 0;
+        rightClick.top = - (canvas.height / 2) + (GUIDE_HEIGHT  / 2) + 30;
+        rightClick.left =  (canvas.width/2) - (GUIDE_WIDTH/2) - 20;
+        rightClick.isEnabled = false;
+        leftClick.width = `${GUIDE_WIDTH}px`;
+        leftClick.height = `${GUIDE_HEIGHT}px`;
+        leftClick.color = "white";
+        leftClick.thickness = 0;
+        leftClick.top = - (canvas.height / 2) + (GUIDE_HEIGHT/2 ) + 30;
+        leftClick.left = - (canvas.width/2) + (GUIDE_WIDTH/2) + 20;
+        leftClick.isEnabled = false;   
+    }
+    function createGuide() {
+        guideLocation();
+        advancedTexture.addControl(rightClick);
+        advancedTexture.addControl(leftClick);
 
+    }
     function getPicker(mesh) {
-        const PICKER_HEIGHT = 200, PICKER_WIDTH = 200;
+        const PICKER_HEIGHT = 220, PICKER_WIDTH = 220;
 
         if (picker == null) {
             pickerTitle = new BABYLON.GUI.TextBlock();
             pickerTitle.text = "";
             pickerTitle.height = "30px";
             pickerTitle.isVisible = false;
+            pickerTitle.color = "white";
                         
             picker = new BABYLON.GUI.ColorPicker();
             //picker.value = skullMaterial.diffuseColor;
@@ -24,7 +50,7 @@ window.addEventListener('DOMContentLoaded', async event => {
             picker.isVisible = false;
 
             advancedTexture.addControl(picker);
-            advancedTexture.addControl(pickerTitle);
+            //advancedTexture.addControl(pickerTitle);
 
         }
         picker.top = `${window.innerHeight / 2 - ((PICKER_HEIGHT * 2))}px`;
@@ -55,14 +81,21 @@ window.addEventListener('DOMContentLoaded', async event => {
         getPicker(mesh).isVisible = false;
         pickerTitle.isVisible = false;
     }
-    function createMenu(button, index) {
+
+    function menuLocation() {
+        const WIDTH = 250, HEIGHT=50;     
+        const style = advancedTexture.createStyle();
+        style.borderRadius = "20px";
+        button.style = style;
         button.width = `${WIDTH}px`;
         button.height = `${HEIGHT}px`;
-        button.color = "white";
-        button.fontSize = 15;
-        button.background = "gray";
-        button.top = (canvas.height / 2) - (HEIGHT * TOP_FACTOR);
-        button.left = index * WIDTH;
+        button.color = "black";
+        button.background= "#eba4c9";
+        button.top = (canvas.height / 2) - (HEIGHT * 2);
+        button.left = (canvas.width/2) - (WIDTH/2) - 50;
+    }
+    function createMenu() {
+        menuLocation();
         button.onPointerClickObservable.add(() => {
             //  window.location.reload();
             BABYLON.GLTF2Export.GLBAsync(scene, "suka").then((glb) => {
@@ -74,9 +107,10 @@ window.addEventListener('DOMContentLoaded', async event => {
 
     function resize(button, index) {
 
-        button.top = (canvas.height / 2)- (HEIGHT * TOP_FACTOR); ;
+       // button.top = (canvas.height / 2)- (HEIGHT * TOP_FACTOR); ;
        // button.left = index * WIDTH;
-
+        menuLocation();
+        guideLocation();
     }
 
     async function createScene() {
@@ -89,8 +123,11 @@ window.addEventListener('DOMContentLoaded', async event => {
             enableGroundShadow: false,
             groundYBias: 1
         });
-        var camera = new BABYLON.ArcRotateCamera("camera1", Math.PI/2, Math.PI/2, 8, new BABYLON.Vector3(0, 0, 0), scene);
+        var camera = new BABYLON.ArcRotateCamera("camera1", Math.PI/2, Math.PI/2, 6.5, new BABYLON.Vector3(0, 0, 0), scene);
 
+        camera.lowerRadiusLimit = 6.5;
+        camera.upperRadiusLimit = 6.5;
+        camera.panningSensibility = 0;
         BABYLON.SceneLoader.ImportMesh("", "assets/glb/", "suka.glb", scene, function (newMeshes, particleSystems, skeletons, animationGroups) {
             newMeshes.forEach(mesh => {
                 if (mesh.name == "__root__") {
@@ -103,13 +140,14 @@ window.addEventListener('DOMContentLoaded', async event => {
         });
         camera.setTarget(BABYLON.Vector3.Zero());
         camera.attachControl(canvas, true);
-        advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-        scene.clearColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+       // advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        scene.clearColor = new BABYLON.Color4(0.03, 0.03, 0.03, 0.5);
 
         return scene;
     };
     await createScene(); 
-    createMenu(button, 0); 
+    createMenu(); 
+    createGuide();
     // Register a render loop to repeatedly render the scene
     engine.runRenderLoop(function () {
         scene.render();
@@ -142,8 +180,9 @@ window.addEventListener('DOMContentLoaded', async event => {
     });   
     
    scene.onKeyboardObservable.add((kbInfo) => {
-        switch (kbInfo.type) {
+        console.log(kbInfo.event);
 
+        switch (kbInfo.type) {
           case BABYLON.KeyboardEventTypes.KEYUP:
             if (kbInfo.event.code == "Escape") {
                 hidePicker();
