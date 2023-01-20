0f03548c18c7857d08c3d0d4f61aea376ca22f91
Ahmed Sayed <ahmad.ahmadsayed@gmail.com>
2023-01-20 22:54:51 +0800

    remove IPFS

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://localhost:3000", "http://127.0.0.1:5001", "https://webui.ipfs.io"]'
-ipfs daemon --enable-pubsub-experiment &
-
-node src/index.js 
