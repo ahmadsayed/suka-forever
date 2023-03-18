549a335f83495cb7c85dbf9c2ad88ee79dfddf44
ahmed <Ahmed.Hassan.Abdelaal@sap.com>
2023-03-19 06:48:17 +0800

    Update FAQ

--- frontend/dist/js/faq.js
+++ frontend/dist/js/faq.js
@@ -5,11 +5,11 @@ window.addEventListener('DOMContentLoaded', event => {
     const faqs = [
         {
             question: "What is this website?",
-            answer: "The current goal is to share and license our digital art as assets. The asset owner will access the original file to use in their project or website."
+            answer: "The main goal is to give them a Git-like experience while collaborating with other artists or developers in creating 3D models or 3D environments."
         },
         {
             question: "Who is Suka?",
-            answer: "Suka and friends are a small community representing diversity. Each one of them has its persona and energy. These characters come together to build a multicultural atmosphere. They can be a part of a game or a puzzle. They can also play a role in an animated movie. Suka and friends are a small community representing diversity."
+            answer: "Suka and friends are a small community representing diversity. Each one of them has its persona and energy. These characters come together to build a multicultural atmosphere. They can be a part ofese characters come together to build a multicultural atmosphere. They can be a part of a game or a puzzle. They can also play a role in an animated movie. S a game or a puzzle. They can also play a role in an animated movie."
         },
         {
             question: "How can I access the assets files?",
@@ -21,11 +21,7 @@ window.addEventListener('DOMContentLoaded', event => {
         },   
         {
             question: "What is the future of SukaVerse?",
-            answer: "We have yet to publish a roadmap. We are enhancing our dApp, enabling users to customize and mint their SUKA. The original SUKA collection is limited to 1000 and will be only our artist's takes on their craft."
-        },
-        {
-            question: "What do we mean by WEB 2.5?",
-            answer: "We use the WEB 3.0 eco-system, currently only Polygon network and IPFS, for identity verification and storing the assets. Still, the website is hosted and managed by SUKA-Verse Team."
+            answer: "We have yet to publish a roadmap. We are enhancing our dApp, enabling users to customize and mint their SUKA"
         }
     ]
     faqs.forEach(faq => {
