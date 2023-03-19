58f52bc3a3339238634e6da04036c14d178f5575
ahmed <Ahmed.Hassan.Abdelaal@sap.com>
2023-03-20 01:12:14 +0800

    FAQ FAQ FAQ

--- frontend/dist/js/faq.js
+++ frontend/dist/js/faq.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', event => {
         {
             question: "Who is Suka?",
             answer: "Suka and friends are \" Matryoshka\" inspired chracters. They are a small community representing diversity. Each one of them has their persona and energy. These characters come together to build a multicultural loving atmosphere.They can be a part of a game or a puzzle. They can also play a role in an animated movie."
-        }
+        },
         {
             question: "What is the future of SukaVerse?",
             answer: "We have yet to publish a roadmap. We are enhancing our dApp, enabling users to customize and mint their SUKA"
